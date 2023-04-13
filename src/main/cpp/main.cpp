f9cb6dc834c48317f1c20d406e2ad28164ea0095
Helen Le <hyle@wpi.edu>
2023-04-12 22:43:54 -0400

    fixed driving functions

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -3,6 +3,7 @@
 #include <frc/Joystick.h>
 
 #include <frc/TimedRobot.h>
+#include <frc/Encoder.h>
 
 #include <cameraserver/CameraServer.h>
 #include <opencv2/core/core.hpp>
@@ -21,6 +22,7 @@
 #include <frc/Timer.h>
 #include <frc2/command/SequentialCommandGroup.h>
 #include <iostream>
+#include <frc/AnalogInput.h>
 
 #include "ctre/Phoenix.h"
 
@@ -31,9 +33,15 @@ using namespace std;
 
 class Robot : public frc::TimedRobot {
 
-  NT_Subscriber ySub;
+  //NT_Subscriber ySub;
+
+  double d  = 0.0;
 
   static const int leftLeadDeviceID = 4, leftFollowDeviceID = 3, rightLeadDeviceID = 2, rightFollowDeviceID = 1;
+
+  frc::Encoder encoder{0,1};
+
+  frc::AnalogInput hall_effect{0};
 
   rev::CANSparkMax m_leftLeadMotor{leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless};
 
@@ -43,8 +51,18 @@ class Robot : public frc::TimedRobot {
 
   rev::CANSparkMax m_rightFollowMotor{rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless};
 
-  TalonSRX linear_slider = {1};
-  TalonSRX excavator = {5};
+  rev::CANSparkMax m_belt{5, rev::CANSparkMax::MotorType::kBrushless};
+
+  TalonSRX linear_slider = {5};
+  TalonSRX excavator = {1}; // double check
+  TalonSRX bucket = {3};
+  bool bucket_dumping_done = 0;
+
+  float initial_bucket_encoder_reading = 0.0;
+  bool bucket_done = 0;
+
+    bool done = 0;
+  
   
   rev::SparkMaxRelativeEncoder leftLead_encoder = m_leftLeadMotor.GetEncoder();
 
@@ -66,11 +84,15 @@ class Robot : public frc::TimedRobot {
   vector<int> speeds;
   
   double default_array[3];
+  std::vector<double> heading = {-1,-1, -1};
+  std::vector<double> coord = {-1,-1, -1};
+  int counter = 0;
 
-  
-  
+// directly horizontal /start position : 1079590912 
+// 
 
-  frc::DifferentialDrive m_robotDrive{m_leftLeadMotor, m_rightLeadMotor};
+
+frc::DifferentialDrive m_robotDrive{m_leftLeadMotor, m_rightLeadMotor};
 // frc::DifferentialDrive::TankDrive m_robotDrive;
  private:
 
@@ -81,6 +103,12 @@ class Robot : public frc::TimedRobot {
     float left_speed = 0.0;
 
     void RobotInit() override{
+
+        encoder.SetDistancePerPulse(0.05);
+        // encoder.SetMinRate(10);
+       // encoder.SetReverseDirection(true);
+        // encoder.SetSamplesToAverage(5);
+
         m_rightFollowMotor.Follow(m_rightLeadMotor, false);
         m_leftFollowMotor.Follow(m_leftLeadMotor, false);
         m_rightLeadMotor.SetInverted(true);
@@ -88,16 +116,17 @@ class Robot : public frc::TimedRobot {
         // units::unit_t<units::time::second, double, units::linear_scale> experation;
         units::second_t experation(1.5);
         m_robotDrive.SetExpiration(experation);
+        linear_slider.SetInverted(true);
         
 
     }
-    //pi per second at top speed
-    //pi/2 per second at half speed
-    //linear
+//     //pi per second at top speed
+//     //pi/2 per second at half speed
+//     //linear
 
-    //encoder resolution: 42
-    //wheelbase: 20inches 
-    //diameter: 10inches 
+//     //encoder resolution: 42
+//     //wheelbase: 20inches 
+//     //diameter: 10inches 
 
    
     void AutonomousPeriodic() override {
@@ -108,23 +137,126 @@ class Robot : public frc::TimedRobot {
         // drive(50);
         // turn(45);
         //negative - up
-        // print_encoders();
-        // excavator.Set(ControlMode::PercentOutput, -2);
-        // linear_slider.Set(ControlMode::PercentOutput, 100);
-        localization();
+       //   bucket.SetInverted(true);  
+        //      bucket.Set(ControlMode::PercentOutput,0.5);
+ 
+ // bucket_function();
+       
+      //  drive(235);
+        drive(20);
+
+    //    excavator.Set(ControlMode::PercentOutput,-20);
+    //    linear_slider.Set(ControlMode::PercentOutput, -20);
+        // printf("Linear Slider\n");
+        // printf( "%d\n",linear_slider.GetSelectedSensorPosition(0));
+        // if(!done) {
+        //     // drive(235);
+
+        // }
+        
+        // if(counter <= 150){
+        //     linear_slider.Set(ControlMode::PercentOutput, 0.3);
+        //    // printf( "%d\n",excavator.GetSelectedSensorPosition(0));
+        //     counter ++ ;
+        // }
+        // else {
+        //     linear_slider.Set(ControlMode::PercentOutput, 0);
+        //     counter = 1;
+        // }
+
+        // if(counter <= 150){
+        //     linear_slider.Set(ControlMode::PercentOutput, 0.3);
+        //    // printf( "%d\n",excavator.GetSelectedSensorPosition(0));
+        //     counter ++ ;
+        // }
+        //  encoder_values();
+        // localization();
        // arc_turn_left(50, 10, 90);
-    //    print_encoders();
+        // m_robotDrive.TankDrive(0.4, 0.0);
+        // m_belt.Set(0.4);
+       // std::cout << hall_effect.GetValue() << std::endl;
+
+        // if(counter == 0){
+        //     counter = 2;
+        //     reconfigure_up();
+        //     reconfigure_down();
+        // }
+        // if(counter == 1){
+        //     counter = 2;
+        // }
+        
+        // sleep(0.4);
+        // linear_slider.Set(ControlMode::PercentOutput, 0.0);
+        // m_belt.Set(-0.3);
+       //bucket_function();
+     
     }
 
+    void reconfigure_up(){
+        int current_encoder = linear_slider.GetSelectedSensorPosition(0);
+        while (linear_slider.GetSelectedSensorPosition(0) < current_encoder + 600){
+            linear_slider.Set(ControlMode::PercentOutput, -0.4);
+        }
+        linear_slider.Set(ControlMode::PercentOutput, 0.0);
+        counter = 1;
+    }
+
+    void reconfigure_down(){
+        int current_encoder = linear_slider.GetSelectedSensorPosition(0);
+        while (linear_slider.GetSelectedSensorPosition(0) > current_encoder - 600){
+            linear_slider.Set(ControlMode::PercentOutput, 0.4);
+        }
+        linear_slider.Set(ControlMode::PercentOutput, 0.0);
+    }
+    
+
+    void bucket_function()
+    {
+        // get encoder values from the bucket
+        
+         initial_bucket_encoder_reading = encoder.GetDistance();
+
+         while(abs(d - 110) > 0 && !bucket_done)
+         {
+            bucket.Set(ControlMode::PercentOutput,1);
+            encoder_values();
+            print_encoders();
+        //    // printf( "%d\n",excavator.GetSelectedSensorPosition(0));
+         };
+     
+           bucket.Set(ControlMode::PercentOutput,0);
+           bucket_dumping_done = 1;
+           sleep(1);
+    
+             if(bucket_dumping_done)
+             {
+                
+            while (abs( d- 5) > 0 )
+            {
+             bucket.SetInverted(true);  
+             bucket.Set(ControlMode::PercentOutput,1);
+             encoder_values();
+             print_encoders();
+            }
+            bucket.Set(ControlMode::PercentOutput,0);
+            bucket_dumping_done=0;
+            bucket_done=1;
+             }
+
+    }
+
+// reading bucket values
+   void encoder_values()
+   {
+        d = encoder.GetDistance();
+        encoder.GetRate();
+   }
 
     //turns until 45 degrees, if cant find april tag then drive forward and repeat
     //once finds april tag, it stops (unless we want it to drive forward a bit/be perpendicular?)
 
     void localization() {
         double dist_goal = 0.5;
-
-
-        std::vector<double> coord = {-1,-1, -1};
 
         coord = frc::SmartDashboard::GetNumberArray("Coordinate Location m", default_array);
         x_curr = coord[0];
@@ -135,7 +267,7 @@ class Robot : public frc::TimedRobot {
         float encoder_cycle = 165;
         while(x_curr == -1 && y_curr == -1) {
             if(leftLead_encoder.GetPosition() - left_encoder_start <= encoder_cycle) {
-                m_robotDrive.TankDrive(0.4, -0.4);
+                // m_robotDrive.TankDrive(0.4, -0.4);
                 coord = frc::SmartDashboard::GetNumberArray("Coordinate Location m", default_array);
                 x_curr = coord[0];
                 y_curr = coord[1];
@@ -173,7 +305,6 @@ class Robot : public frc::TimedRobot {
             linear_diff = abs(sqrt(pow((x_goal - x_curr), 2) + pow((y_goal - y_curr), 2)));
             float target_angle =  atan2((y_goal - y_curr), (x_goal - x_curr)) + 4*M_PI;
             //this can be implemented or pulled somehow from localization once I figure out how it works
-            std::vector<double> heading = {-1,-1, -1};
             heading = frc::SmartDashboard::GetNumberArray("Heading Deg (Z,Y,X)", default_array);
 
             float current_angle = heading[0];
@@ -292,50 +423,53 @@ class Robot : public frc::TimedRobot {
 
 
     bool drive (float distance) {
-        
-        double encoder_goal = leftLead_encoder.GetPosition() + (distance/(3.14*wheel_diam) * enc_res);
-        
-        if(encoder_goal > 0) {
+        sleep(3);
+        double encoder_goal = (distance)/(3.14*wheel_diam) * enc_res * 3.95 + leftLead_encoder.GetPosition();
+
+        if(distance > 0) {
+
             float speed = 0.5;
-            std::cout << "GETPOSITION " << leftLead_encoder.GetPosition() << " ENCODERGOAL " << encoder_goal << std::endl;
-            while((leftLead_encoder.GetPosition() <= encoder_goal + 50 || leftLead_encoder.GetPosition() <=encoder_goal - 50) && (rightLead_encoder.GetPosition() <= encoder_goal + 50 || rightLead_encoder.GetPosition() <= encoder_goal- 50)) {
+            while((leftLead_encoder.GetPosition() <= encoder_goal + 5 || leftLead_encoder.GetPosition() <=encoder_goal - 5) && (rightLead_encoder.GetPosition() <= encoder_goal + 5 || rightLead_encoder.GetPosition() <= encoder_goal- 5)) {
                 double errorLeft = encoder_goal - leftLead_encoder.GetPosition();
                 double errorRight = encoder_goal - rightLead_encoder.GetPosition();
-                int k = 0.5;
-                // std::cout << "GETPOSITION " << leftLead_encoder.GetPosition() << " ENCODERGOAL " << encoder_goal << std::endl;
+                int k = 0.2;
                 // m_leftLeadMotor.Set(speed - (errorLeft * k));
                 float speedL = speed - (errorLeft * k);
                 // m_rightLeadMotor.Set(speed - (errorRight * k));
                 float speedR = speed - (errorRight * k);
-                // std::cout << "GETPOSITION " << leftLead_encoder.GetPosition() << " ENCODERGOAL " << encoder_goal << std::endl;
-                // std::cout << "SPEED " << speedL << std::endl;
                 m_robotDrive.TankDrive(speedL,speedR);
+
             }
             // m_leftLeadMotor.Set(0);
             // m_rightLeadMotor.Set(0);
             m_robotDrive.TankDrive(0.0,0.0);
             sleep(3);
-            std::cout << "RETURNING" << std::endl;
-
+            done = 1;
             return true;
         }
         else {
-            float speed = 0.3;
-            while((leftLead_encoder.GetPosition() >= encoder_goal + 50 || leftLead_encoder.GetPosition() >= encoder_goal - 50) && (rightLead_encoder.GetPosition() >= encoder_goal + 50 || rightLead_encoder.GetPosition() >= encoder_goal - 50)) {
-                int errorLeft = abs(encoder_goal) - abs(leftLead_encoder.GetPosition());
-                int errorRight = abs(encoder_goal) - abs(rightLead_encoder.GetPosition());
+            
+            float speed = -0.5;
+            m_robotDrive.TankDrive(speed, speed);
+
+            while(encoder_goal - leftLead_encoder.GetPosition() <=0) {    
+                double errorLeft = encoder_goal - leftLead_encoder.GetPosition();
+                double errorRight = encoder_goal - rightLead_encoder.GetPosition();
+                // double errorLeft = abs(encoder_goal) - abs(leftLead_encoder.GetPosition());
+                // int errorRight = abs(encoder_goal) - abs(rightLead_encoder.GetPosition());
                 int k = 0.2;
 
                 // m_leftLeadMotor.Set(-speed - (errorLeft * k));
-                float speedL = -speed - (errorLeft * k);
+                float speedL = speed + (errorLeft * k);
                 // m_rightLeadMotor.Set(-speed - (errorRight * k));
-                float speedR = -speed - (errorRight * k);
+                float speedR = speed + (errorLeft * k);
                 m_robotDrive.TankDrive(speedL, speedR);
 
             }
             // m_leftLeadMotor.Set(0);
             // m_rightLeadMotor.Set(0);
             m_robotDrive.TankDrive(0.0,0.0);
+            done = 1;
             return true;
 
         }
@@ -347,10 +481,13 @@ class Robot : public frc::TimedRobot {
         // frc::SmartDashboard::PutNumber("Left Follower Position", leftFollower_encoder.GetPosition());
         // frc::SmartDashboard::PutNumber("Right Follower Position", rightLead_encoder.GetPosition());
 
-        double default_array [3];
-        // std::vector<int> v = {0, 0, 0};
-        // default_array(v);          // OK
-
+       // double default_array [3];
+      //   std::vector<int> v = {0, 0, 0};
+       //  default_array(v);          // OK
+       frc::SmartDashboard::PutNumber("Encoding Scale",encoder.GetEncodingScale());
+       frc::SmartDashboard::PutNumber("DumpingBoolean",bucket_dumping_done);
+       frc::SmartDashboard::PutNumber("InitialEncoderValues",initial_bucket_encoder_reading);
+       frc::SmartDashboard::PutNumber("Encoder",d);
 
     }
 };
